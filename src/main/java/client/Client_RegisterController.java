960fcb4177edb9ba963c5736aa2c6898c6644898
ilaila05 <zorzellailaria1ci@gmail.com>
2023-12-14 19:05:06 +0100

    controller register fatto

--- src/main/java/client/Client_RegisterController.java
+++ src/main/java/client/Client_RegisterController.java
@@ -1,4 +1,70 @@
 package client;
 
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Client_RegisterController {
+    @FXML
+    private MFXTextField nome;
+    @FXML
+    private MFXTextField cognome;
+    @FXML
+    private MFXTextField nickname;
+    @FXML
+    private MFXTextField password;
+    @FXML
+    private MFXButton registrati;
+    @FXML
+    private MFXButton login;
+
+    public void initialize() {
+        registrati.setOnAction(event -> invio());
+        login.setOnAction(event -> openLogin());
+    }
+
+    public void invio(){
+        String sNome = nome.getText();
+        String sCognome= cognome.getText();
+        String sNickname = nickname.getText();
+        String sPassword = password.getText();
+
+        if(sNome.equals("") || sCognome.equals("") || sNickname.equals("") || sPassword.equals("")){
+            System.out.println("no");
+        }
+
+        System.out.println(sNome);
+        System.out.println(sCognome);
+        System.out.println(sNickname);
+        System.out.println(sPassword);
+    }
+
+    private void openLogin() {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/login.fxml"));
+        try {
+            VBox loginLayout = fxmlLoader.load();
+
+            Stage loginStage = new Stage();
+            loginStage.initModality(Modality.APPLICATION_MODAL);
+            loginStage.setTitle("Registrati");
+
+            Scene chatListScene = new Scene(loginLayout);
+            loginStage.setScene(chatListScene);
+
+            Scene currentScene = login.getScene();
+            Stage currentStage = (Stage) currentScene.getWindow();
+            currentStage.close();
+
+            loginStage.show();
+        } catch ( IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
